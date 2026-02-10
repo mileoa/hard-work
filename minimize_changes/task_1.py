6c83e3c6fe9bfec099fcf52a316632ab453d8c89
mileoa <ezkimozzzzz@gmail.com>
2026-02-10 22:40:52 +1000

    3

--- minimize_changes/task_1.py
+++ minimize_changes/task_1.py
@@ -1,10 +1,21 @@
 from copy import deepcopy
 
 
+class PromoCode:
+    def __init__(self, price: float):
+        if price <= 0:
+            raise ValueError("Price must be > 0")
+        self._price: float = price
+
+    @property
+    def price(self):
+        return self._price
+
+
 class DiscountPercentagesByPrice:
     def __init__(self, price_from: float, discount_percentages: int):
         if price_from < 0:
-            raise ValueError("Amount can not be negative")
+            raise ValueError("Price can not be negative")
         if discount_percentages < 0:
             raise ValueError("Percentages can not be negative")
 
@@ -22,12 +33,19 @@ class DiscountPercentagesByPrice:
 
 class DiscountCalculator:
 
-    def __init__(self, percetages_by_price: list[DiscountPercentagesByPrice]):
+    def __init__(
+        self,
+        max_discount_percetages: int,
+        percetages_by_price: list[DiscountPercentagesByPrice],
+    ):
+        if max_discount_percetages < 0:
+            raise ValueError("max_discount_percetages can not be negative")
         self._percetages_by_price_discount: list[DiscountPercentagesByPrice] = (
             self._arrange_percentages_by_price(percetages_by_price)
         )
+        self._max_discount_percetages: int = max_discount_percetages
 
-    def calulate_discount(self, price: float) -> float:
+    def calulate_discount(self, price: float, promo_code: PromoCode = None) -> float:
         total_discount: float = 0.0
         for percetages_by_price_discount in self._percetages_by_price_discount:
             if price >= percetages_by_price_discount.price_from:
@@ -35,6 +53,14 @@ class DiscountCalculator:
                     price / 100 * percetages_by_price_discount.discount_percentages
                 )
                 break
+
+        if promo_code is not None:
+            total_discount += promo_code.price
+
+        total_discount = min(
+            total_discount, price / 100 * self._max_discount_percetages
+        )
+
         return total_discount
 
     def _arrange_percentages_by_price(
--- minimize_changes/test_1.py
+++ minimize_changes/test_1.py
@@ -1,5 +1,5 @@
 import unittest
-from task_1 import DiscountCalculator, DiscountPercentagesByPrice
+from task_1 import DiscountCalculator, DiscountPercentagesByPrice, PromoCode
 
 
 class TestDiscountCalculator(unittest.TestCase):
@@ -10,7 +10,9 @@ class TestDiscountCalculator(unittest.TestCase):
             DiscountPercentagesByPrice(price_from=5000.0, discount_percentages=10),
             DiscountPercentagesByPrice(price_from=1000.0, discount_percentages=5),
         ]
-        self.calculator = DiscountCalculator(discount_percentages)
+        self.calculator = DiscountCalculator(
+            max_discount_percetages=50, percetages_by_price=discount_percentages
+        )
 
     def test_without_discounts(self):
         discount = self.calculator.calulate_discount(price=500.00)
@@ -29,6 +31,30 @@ class TestDiscountCalculator(unittest.TestCase):
         assert discount_3 == 200.00
         assert discount_4 == 500.00
         assert discount_5 == 1000.00
+
+    def test_discount_with_promo(self):
+        discount = self.calculator.calulate_discount(
+            price=100.00, promo_code=PromoCode(50)
+        )
+
+        assert discount == 50.00
+
+    def test_promo_greater_price(self):
+        self.calculator = DiscountCalculator(
+            max_discount_percetages=100, percetages_by_price=[]
+        )
+        discount = self.calculator.calulate_discount(
+            price=100.00, promo_code=PromoCode(101.00)
+        )
+
+        assert discount == 100.00
+
+    def test_discount_greater_max_discount(self):
+        discount = self.calculator.calulate_discount(
+            price=100.00, promo_code=PromoCode(101.00)
+        )
+
+        assert discount == 50.00
 
 
 if __name__ == "__main__":
