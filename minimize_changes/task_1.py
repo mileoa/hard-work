0763ae3b8e6346c87fbfa7aff3aeae033e2db6a8
mileoa <ezkimozzzzz@gmail.com>
2026-02-10 22:25:11 +1000

    2

--- minimize_changes/task_1.py
+++ minimize_changes/task_1.py
@@ -1,4 +1,47 @@
+from copy import deepcopy
+
+
+class DiscountPercentagesByPrice:
+    def __init__(self, price_from: float, discount_percentages: int):
+        if price_from < 0:
+            raise ValueError("Amount can not be negative")
+        if discount_percentages < 0:
+            raise ValueError("Percentages can not be negative")
+
+        self._price_from: float = price_from
+        self._discount_percentages: int = discount_percentages
+
+    @property
+    def price_from(self):
+        return self._price_from
+
+    @property
+    def discount_percentages(self):
+        return self._discount_percentages
+
+
 class DiscountCalculator:
 
-    def calulate_discount(self, amount):
-        return 0
+    def __init__(self, percetages_by_price: list[DiscountPercentagesByPrice]):
+        self._percetages_by_price_discount: list[DiscountPercentagesByPrice] = (
+            self._arrange_percentages_by_price(percetages_by_price)
+        )
+
+    def calulate_discount(self, price: float) -> float:
+        total_discount: float = 0.0
+        for percetages_by_price_discount in self._percetages_by_price_discount:
+            if price >= percetages_by_price_discount.price_from:
+                total_discount += (
+                    price / 100 * percetages_by_price_discount.discount_percentages
+                )
+                break
+        return total_discount
+
+    def _arrange_percentages_by_price(
+        self, percetages_by_price: list[DiscountPercentagesByPrice]
+    ) -> list[DiscountPercentagesByPrice]:
+        percetages_by_price_sorted: list[DiscountPercentagesByPrice] = deepcopy(
+            percetages_by_price
+        )
+        percetages_by_price_sorted.sort(key=lambda x: x.price_from, reverse=True)
+        return percetages_by_price_sorted
--- minimize_changes/test_1.py
+++ minimize_changes/test_1.py
@@ -1,17 +1,34 @@
 import unittest
-from task_1 import DiscountCalculator
+from task_1 import DiscountCalculator, DiscountPercentagesByPrice
 
 
 class TestDiscountCalculator(unittest.TestCase):
 
     def setUp(self):
-        self.calculator = DiscountCalculator()
+        discount_percentages = [
+            DiscountPercentagesByPrice(price_from=0.0, discount_percentages=0),
+            DiscountPercentagesByPrice(price_from=5000.0, discount_percentages=10),
+            DiscountPercentagesByPrice(price_from=1000.0, discount_percentages=5),
+        ]
+        self.calculator = DiscountCalculator(discount_percentages)
 
     def test_without_discounts(self):
-
-        discount = self.calculator.calulate_discount(amount=500.00)
+        discount = self.calculator.calulate_discount(price=500.00)
 
         assert discount == 0.00
+
+    def test_discount_percentages(self):
+        discount_1 = self.calculator.calulate_discount(price=100.00)
+        discount_2 = self.calculator.calulate_discount(price=1000.00)
+        discount_3 = self.calculator.calulate_discount(price=4000.00)
+        discount_4 = self.calculator.calulate_discount(price=5000.00)
+        discount_5 = self.calculator.calulate_discount(price=10000.00)
+
+        assert discount_1 == 0.00
+        assert discount_2 == 50.00
+        assert discount_3 == 200.00
+        assert discount_4 == 500.00
+        assert discount_5 == 1000.00
 
 
 if __name__ == "__main__":
